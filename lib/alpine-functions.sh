98073199896318cfa139a3b885e1de8da1ec986f
Sascha Wander <sascha.wander@gmail.com>
2018-08-10 19:26:00 +0200

    foo

--- lib/alpine-functions.sh
+++ lib/alpine-functions.sh
@@ -1,13 +1,20 @@
 
-die() {
-	printf '\033[1;31mERROR:\033[0m %s\n' "$@" >&2  # bold red
-	exit 1
+die () {
+    local ret="$?"
+	printf '\033[1;31mERROR:\033[0m %s\n' "$@" >&2
+	exit "$?"
 }
 
-einfo() {
-	printf '\n\033[1;36m> %s\033[0m\n' "$@" >&2  # bold cyan
+einfo () {
+	printf '\n\033[1;36m> %s\033[0m\n' "$@" >&2
 }
 
-ewarn() {
-	printf '\033[1;33m> %s\033[0m\n' "$@" >&2  # bold yellow
+ewarn () {
+	printf '\033[1;33m> %s\033[0m\n' "$@" >&2
 }
+
+debug () {
+    [[ -n "$DEBUG" ]] && einfo "$@"
+}
+
+# vim:fenc=utf-8:ft=sh:
