77ef490ed1c5804bdbd29fb0ac37944bc677fdd8
Sascha Wander <sascha.wander@gmail.com>
2018-08-20 08:04:41 +0200

    removed executable privileges in lib

--- .shared_rc.d/pass.sh
+++ .shared_rc.d/pass.sh
@@ -1,15 +1,22 @@
 # $Id: ~/.shared_rc.d/pass-teaming.sh wandsas 2018/08/20
 
+info () {
+	printf '\n\033[1;36mINFO\033[0m %s\n' "$@" >&2
+}
+
 pass-default () {
     export PASSWORD_STORE_DIR="$HOME/.pass-wandsas"
+    info "PASSWORD_STORE_DIR=$PASSWORD_STORE_DIR"
 }
 
 pass-android () {
     export PASSWORD_STORE_DIR="$HOME/.pass-android"
+    info "PASSWORD_STORE_DIR=$PASSWORD_STORE_DIR"
 }
 
 pass-aladdin () {
     export PASSWORD_STORE_DIR="$HOME/.pass-aladdin"
+    info "PASSWORD_STORE_DIR=$PASSWORD_STORE_DIR"
 }
 
 [[ -z "$PASSWORD_STORE_DIR" ]] && pass-default
---
+++ bin/lukscontainer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BASEDIR=$(xdg-user-dir DOCUMENTS)
+
+open_lukscontainer () {
+    sudo cryptsetup luksOpen ${BASEDIR}/.lukscontainer.img lukscontainer
+    install -m 755 -d ${BASEDIR}/lukscontainer
+    sudo chown wandsas:wandsas ${BASEDIR}/lukscontainer
+    sudo mount /dev/mapper/lukscontainer ${BASEDIR}/lukscontainer
+}
+
+close_lukscontainer () {
+    sudo umount ${BASEDIR}/lukscontainer
+    rmdir ${BASEDIR}/lukscontainer
+    sudo cryptsetup luksClose lukscontainer
+}
+
+main () {
+    # Toggle open/close container
+    if mountpoint -q ${BASEDIR}/lukscontainer >/dev/null 2>&1; then
+        close_lukscontainer
+    else
+        open_lukscontainer
+    fi
+}
+main "$@"
+
+# vim:fenc=utf-8:ft=sh:
